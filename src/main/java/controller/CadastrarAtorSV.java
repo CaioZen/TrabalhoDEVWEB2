1a73eddb2c6d33217d8fa9f0c36c92ebc445eae0
CaioZen <caiozengoncalves@gmail.com>
2025-08-26 11:39:59 -0300

    :sparkles: hibernate cfg and implementation for 'Ator'

--- src/main/java/controller/CadastrarAtorSV.java
+++ src/main/java/controller/CadastrarAtorSV.java
@@ -6,12 +6,32 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import model.domain.Ator;
+import model.domain.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 @WebServlet("/CadastrarAtorSV")
 public class CadastrarAtorSV extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String nomeAtor = request.getParameter("nomeAtor");
-        Ator ator = new Ator(nomeAtor);
-        System.out.println("Ator recebido: " + ator.getNomeAtor());
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+
+        try{
+            transaction = session.beginTransaction();
+            Ator ator = new Ator(nomeAtor);
+            session.save(ator);
+            transaction.commit();
+            System.out.println("Ator cadastrado com sucesso!");
+        } catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+                System.out.println("Erro ao cadastrar Ator!");
+            }
+        } finally{
+            session.close();
+            response.sendRedirect("index.jsp");
+        }
+
     }
 }
--- src/main/java/model/domain/Ator.java
+++ src/main/java/model/domain/Ator.java
@@ -4,9 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Ator {
+@Entity
+@Table
+public class Ator implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idAtor;
+    @Column(nullable = false, length = 100)
     private String nomeAtor;
+
+    public Ator(String nomeAtor) {
+        this.nomeAtor = nomeAtor;
+    }
 }
